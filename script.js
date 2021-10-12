abfe9e4f908fa84175510b3a81ad4c9d3023ebef
UfukGursoyTR <91892743+UfukGursoyTR@users.noreply.github.com>
2021-10-12 14:33:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let myText = document.getElementById("my-text");
+
+myText.addEventListener("input", () => {
+  let count = myText.value.length;
+  console.log(myText.value, count);
+  document.getElementById(
+    "count-result"
+  ).textContent = `Total Characters: ${count}`;
+});
